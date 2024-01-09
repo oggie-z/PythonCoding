d92575abb7f5c4d363fecde9651169fa5726ef48
oggie-z <christianoginni@hotmail.com>
2024-01-09 12:07:02 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+
+    # initialise book list
+    bookList = []
+
+
+    choice = 0 
+    while choice !=4:
+    print("*** Cinco Manager ***")
+    print("1) Spam")
+    #print("2) ")
+    #print("3) ")
+    print("4) Quit")
+    choice =int(input())
+
+    if choice == 1:
+        print(("Adding a book..."))
+        nBook = input("Enter the name of the book \
+        >>>")
+        nAuthor = input("Enter the name of the book \
+        >>>")
+        nPages = input("Enter the name of the book \
+        >>>")  
+    
+
+
+
+if __name__ == "__main__":
+    main()
